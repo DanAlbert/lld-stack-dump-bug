81dec95bae3eade7958defa0973b40b046181e85
Dan Albert <danalbert@google.com>
2020-06-25 14:21:25 -0700

    Add intermediates for inspection, built with r21d.

---
+++ build-lld/CMakeFiles/3.16.3/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-5.2.17-1rodete3-amd64")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "5.2.17-1rodete3-amd64")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/usr/local/google/home/danalbert/src/android-ndk-r21d/build/cmake/android.toolchain.cmake")
+
+set(CMAKE_SYSTEM "Android-1")
+set(CMAKE_SYSTEM_NAME "Android")
+set(CMAKE_SYSTEM_VERSION "1")
+set(CMAKE_SYSTEM_PROCESSOR "aarch64")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
